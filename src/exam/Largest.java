659c6b1d7ff6b61d153d126d62819ed80db7dfeb
artursziemelis <arturs.ziemelis@inbox.lv>
2023-06-13 13:01:04 +0300

    Largest started

---
+++ src/exam/Largest.java
@@ -0,0 +1,22 @@
+package exam;
+
+public class Largest {
+    public static void main(String[] args) {
+        int firstNumber = 3;
+        int secondNumber = 4;
+        int thirdNumber = 5;
+        System.out.println("Sum of two largest numbers - " + sumOfTwoLargestNumbers(3,4,5));
+    }
+    public static int sumOfTwoLargestNumbers(int num1, int num2, int num3) {
+        int largest = Math.max(num1, Math.max(num2, num3));
+        int secondLargest;
+        if (num1 == largest) {
+            secondLargest = Math.max(num2, num3);
+        } else if (num2 == largest) {
+            secondLargest = Math.max(num1, num3);
+        } else {
+            secondLargest = Math.max(num1, num2);
+        }
+        return largest + secondLargest;
+    }
+}
