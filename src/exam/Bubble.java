837bd1e82dd557047e522d7be0b5feeea0bd2384
artursziemelis <arturs.ziemelis@inbox.lv>
2023-06-13 12:52:31 +0300

    bubble refactored

--- src/exam/Bubble.java
+++ src/exam/Bubble.java
@@ -2,7 +2,7 @@ package exam;
 
 import java.util.Arrays;
 
-public class bubble {
+public class Bubble {
     public static void main(String[] args) {
         int[] array = {5, 6, 4, 3, 2, 7};
         System.out.println("Not sorted - " + Arrays.toString(array));
