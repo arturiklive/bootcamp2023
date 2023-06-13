997b32e8d933ee610f1a0ddf339708a8329664b9
artursziemelis <arturs.ziemelis@inbox.lv>
2023-06-13 12:45:48 +0300

    bubble added

---
+++ src/exam/bubble.java
@@ -0,0 +1,23 @@
+package exam;
+
+import java.util.Arrays;
+
+public class bubble {
+    public static void main(String[] args) {
+        int[] array = {5, 6, 4, 3, 2, 7};
+        System.out.println("Not sorted - " + Arrays.toString(array));
+        System.out.println("Sorted - " + Arrays.toString(selectionSort(array)));
+    }
+    public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tempElement = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tempElement;
+                }
+            }
+        }
+        return array;
+    }
+}
